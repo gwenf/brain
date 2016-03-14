02aaffee8ce3d0a5a94a146a80323c1e69d18222
Gwendolyn Faraday <gwenfaraday@gmail.com>
2016-03-14 17:07:10 -0400

    fixed routing

--- scripts/app.js
+++ scripts/app.js
@@ -8,10 +8,26 @@ angular.module('brainApp',[
 
     $stateProvider.state('home',{
       url: '/',
-      templateUrl: '../templates/home.html'
+      templateUrl: 'templates/home.html'
     });
     $stateProvider.state('terms',{
       url: '/terms',
-      templateUrl: '../templates/terms.html'
+      templateUrl: 'templates/terms.html'
     });
+    $stateProvider.state('ingredients',{
+      url: '/ingredients',
+      templateUrl: 'templates/ingredients.html'
+    });
+    $stateProvider.state('privacy',{
+      url: '/privacy',
+      templateUrl: 'templates/privacy.html'
+    });
+    // $stateProvider.state('why', {
+    //     url: '/why',
+    //     template: '<h1>This Is A State</h1>'
+    // });
+    // $stateProvider.state('how', {
+    //     url: '/how',
+    //     template: '<h1>This Is A State</h1>'
+    // });
   }])
